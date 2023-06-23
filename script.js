90aac10aa8327dbf2c162bda451a25caae69878b
ArineLakshmiSaiKumar <99064707+ArineLakshmiSaiKumar@users.noreply.github.com>
2023-06-23 12:10:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Tic-Tac-Toe")
+let music = new Audio("music.mp3")
+let turn1 = new Audio("ting.mp3")
+let gameOver = new Audio("gameover.mp3")
+
+let turn = "X";
+//Function to change turn
+const changeTurn = ()=>{
+    return turn === "X"?"0":"X"
+}
+//Function for check a win
+const checkWin = ()=>{
+
+}
+
+//Game logic
